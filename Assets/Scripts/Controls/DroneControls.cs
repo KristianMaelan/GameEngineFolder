6843d730f5bd9a59121396cf8f148df973479d9c
kristianmaelan <89403421+KristianMaelan@users.noreply.github.com>
2023-12-01 11:56:36 +0100

    Class1.December

--- Assets/Scripts/Controls/DroneControls.cs
+++ Assets/Scripts/Controls/DroneControls.cs
@@ -8,6 +8,8 @@ public class DroneControls : MonoBehaviour
 {
     Gamepad controller;
     [SerializeField] private float RotationScale = 2000;
+    private float originalRotScale;
+    private bool _BorignalRotScale = true;
 
     private Transform m_transform;
     
@@ -17,15 +19,40 @@ public class DroneControls : MonoBehaviour
         Debug.Log("Calling override");
         controller = DS4.getController();
         m_transform = this.transform;
+
+        //Setting a fallback rotation scale.
+        originalRotScale = RotationScale;
     }
 
     private void Update()
     {
-
+    //Controls handling
+    {
         if (controller.buttonWest.isPressed)
         {
             m_transform.rotation = Quaternion.identity;
         }
-        m_transform.rotation *= DS4.GetRotation(RotationScale * Time.deltaTime);
+        else if (controller.buttonSouth.wasPressedThisFrame)
+        {
+            if (_BorignalRotScale)
+            {
+                RotationScale *= 0.10f;
+                _BorignalRotScale = false;q
+            }
+            else
+            {
+                RotationScale = originalRotScale;
+                _BorignalRotScale = true;
+            }
+        }
+        else if (controller.buttonNorth.isPressed)
+        {
+            return;
+        }
+    }
+    
+    
+    
+    m_transform.rotation *= DS4.GetRotation(RotationScale * Time.deltaTime);
     }
 }
